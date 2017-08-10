a420045f8ebaff38dc2c925a90ded798bc336a3e
JSpalas desktop <elgranpaicron@gmail.com>
2017-08-09 20:09:35 -0500

    mejora control de nave

--- Assets/bosspater.cs
+++ Assets/bosspater.cs
@@ -4,31 +4,36 @@ using UnityEngine;
 using UnityEngine.AI;
 public class bosspater : MonoBehaviour {
 
-    public ForceMode force;
-    private GameObject player;
-    private Transform trans;
-    public Animator aniamte;
-    private Rigidbody rb;
-    private CharacterController controles;
-    public GameObject check;
-    [SerializeField] private bool lookat;
-    public float back;
-    public GameObject bakctarget;
+    [SerializeField] private int atcconter = 0;
+    [SerializeField] private bool attaccc;
     [SerializeField] private Collider[] col;
     [SerializeField] private Collider[] col1;
-    public LayerMask lay;
-    public float speed ;
+    [SerializeField] private bool charge;
+    [SerializeField] private bool still;
+    private Vector3 charPost;
     private float at;
-    private bool dash=false;
+    private bool dash = false;
     private int dashcont = 0;
-    [SerializeField] private int atcconter = 0;
-    [SerializeField] private bool attaccc = false;
+    private GameObject player;
+    private Transform trans;
+    private Rigidbody rb;
+    public float speed;
+    public ForceMode force;
+    public Animator aniamte;
+    public GameObject check;
+    public float back;
+    public GameObject bakctarget;
+    public LayerMask lay;
+
+
     // Use this for initialization
     void Start () {
         player = GameObject.Find("Player");
         trans = GetComponent<Transform>();
- 
-        controles = GetComponent<CharacterController>();
+        attaccc = false;
+        charge = false;
+        charPost = Vector3.zero;
+        still = false;
         rb = GetComponent<Rigidbody>();
     }
 	
@@ -37,12 +42,10 @@ public class bosspater : MonoBehaviour {
 
         col = Physics.OverlapSphere(trans.position, 10f, lay);
         col1 = Physics.OverlapSphere(trans.position, 1f, lay);
-        if (col.Length != 0 && col1.Length == 0)
+        if (col.Length != 0 && col1.Length == 0 && !charge && !still)
         {
             Vector3 look = new Vector3(player.transform.position.x, trans.position.y, player.transform.position.z);
             trans.LookAt(look);
-            trans.position += trans.forward * Time.deltaTime * speed;
-            // nav.SetDestination(player.transform.position);
             aniamte.SetBool("move", true);
            
         }
@@ -50,7 +53,7 @@ public class bosspater : MonoBehaviour {
         {
             aniamte.SetBool("move", false);
         }
-        if(col1.Length!=0 && !attaccc)
+        if(col1.Length!=0 && !attaccc && !dash)
         {
             aniamte.SetBool("attack", true);
             attaccc = true;
@@ -63,6 +66,7 @@ public class bosspater : MonoBehaviour {
 
         if(dash)
         {
+            aniamte.SetBool("jump", true);
             Vector3 vec = bakctarget.GetComponent<Transform>().position;
             float speee = 23 * Time.deltaTime;
             trans.position = Vector3.MoveTowards(trans.position, vec, speee);
@@ -70,6 +74,22 @@ public class bosspater : MonoBehaviour {
             if(dashcont==15)
             {
                 dash = false;
+                dashcont = 0;
+                still = true;
+               
+                StartCoroutine(chargueIn());
+            }
+        }
+        if (charge)
+        {
+            float speee = 15 * Time.deltaTime;
+            trans.position = Vector3.MoveTowards(trans.position, charPost, speee);
+            if(trans.position==charPost)
+            {
+                charge = false;
+                rb.AddExplosionForce(10f, trans.position, 20,1, ForceMode.Impulse);
+               
+                charPost = Vector3.zero;
             }
         }
 	}
@@ -79,12 +99,11 @@ public class bosspater : MonoBehaviour {
         AnimatorStateInfo aninfo = aniamte.GetCurrentAnimatorStateInfo(0);
         AnimatorClipInfo[] myclip = aniamte.GetCurrentAnimatorClipInfo(0);
         at = myclip[0].clip.length * aninfo.normalizedTime + 0.4f;
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.4f);
         attaccc = false;
-        if(atcconter>=1)
+        if(atcconter>=2)
         {
             dash = true;
-            jumpbak();
             atcconter = 0;
     
         }
@@ -94,16 +113,16 @@ public class bosspater : MonoBehaviour {
         }
     }
 
-    private void jumpbak()
+    public IEnumerator chargueIn()                                          //metodo que guarda la poscion del jugador antes de uniciar el chargue y luego de T segundos inicia el chargue
     {
-      //  Vector3 vec = bakctarget.GetComponent<Transform>().position;
-        //rb.AddForce(Vector3.up * 5, force);
-        // rb.AddForce(vec*-6, force);
-       // float speee = 23 * Time.deltaTime;
-        //trans.position = Vector3.MoveTowards(trans.position, vec, speee);
-        //    GetComponent<Rigidbody>().AddForce(Vector3.back * (-back), force);
-
-
-
+        if(charPost==Vector3.zero)
+        {
+            charPost = player.transform.position;
+        }
+        yield return new WaitForSecondsRealtime(1.5f);
+        aniamte.SetBool("jump", false);
+        charge = true;
+        still = false;
     }
+  
 }
